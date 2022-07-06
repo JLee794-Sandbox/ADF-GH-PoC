f8251ddba8cb2dc82cf006d2115e9573bf567950
Jin Lee <jinle@microsoft.com>
2022-07-06 10:40:58 -0500

    pwsh script fix, gh action debug

--- extract-pipeline-from-trigger.ps1
+++ extract-pipeline-from-trigger.ps1
@@ -2,14 +2,50 @@
 # Lookup pipeline name(s) in the various trigger json documents
 #
 
+function constructPipelineDeployment($targetPipeline) {
+    Get-ChildItem $triggerDirectory -Filter *.json |
+
+
+    Foreach-Object {
+        $content = Get-Content $_.FullName
+        $jsonContent = $content | ConvertFrom-Json
+        $triggerName = $jsonContent.name
+        $props = $jsonContent.properties
+    
+        if ($props.pipelines  | where { $_.pipelineReference.referenceName -eq $targetPipeline }) {
+            Write-Host -ForegroundColor Green "$($_.Name) contains $targetPipeline"
+            # $triggerArr += $content | ConvertFrom-Json | select name
+            Write-Host -ForegroundColor Yellow "Adding trigger: $triggerName to ADF publish options for stop/start"
+            $opt.Includes.Add("trigger.$triggerName", "")
+        }
+        else {
+            Write-Debug "$($_.Name) DOES NOT contain $targetPipeline"
+        }    
+    }
+    
+    $opt.DoNotStopStartExcludedTriggers = $true
+    $opt.DoNotStopStartExcludedTriggers = $true
+
+    return $opt
+}
+function installAdfDependencies {
+    Install-Module Az.DataFactory -MinimumVersion "1.10.0" -Force
+    Install-Module -Name "azure.datafactory.tools" -Force
+}
+try {
+    Import-Module -Name "azure.datafactory.tools"
+} 
+catch {
+    installAdfDependencies
+}
+
 $triggerDirectory="./root/trigger"
 $pipelineDirectory="./root/pipeline"
 $manifestDirectory="./manifest"
 
 $rootFolder="root"
 $adfName = "sqlplayer-poc"
-$adfResourceGroup = "adf-sandbox"
-$adfId = "/subscriptions/3e40d030-d17d-47ba-89bb-79e718105332/resourceGroups/adf-sandbox/providers/Microsoft.DataFactory/factories/sqlplayer-poc"
+$adfResourceGroup = "mgb-adf-sandbox"
 $location = "eastus"
 
 $triggerArr = @()
@@ -17,47 +53,21 @@ $triggerArr = @()
 $opt = New-AdfPublishOption
 
 
-$targetPipeline = Read-Host -Prompt 'Input the target pipeline name (default: "pipeline1")'
+$pipeline = Read-Host -Prompt 'Input the target pipeline name (default: "pipeline1")'
 
-if (!$targetPipeline) {
-    $targetPipeline = "pipeline1"
+if (!$pipeline) {
+    $pipeline = "pipeline1"
 }
 
-Write-Host "The entered pipeline is" $targetPipeline -ForegroundColor Green
+Write-Host "The entered pipeline is" $pipeline -ForegroundColor Green
 
-Get-ChildItem $triggerDirectory -Filter *.json |
+$opt = constructPipelineDeployment($pipeline)
 
-
-Foreach-Object {
-    $content = Get-Content $_.FullName
-    $jsonContent = $content | ConvertFrom-Json
-    $triggerName = $jsonContent.name
-    $props = $jsonContent.properties
-
-    if ($props.pipelines  | where { $_.pipelineReference.referenceName -eq $targetPipeline }) {
-        Write-Host -ForegroundColor Green "$($_.Name) contains $targetPipeline"
-        # $triggerArr += $content | ConvertFrom-Json | select name
-        Write-Host -ForegroundColor Yellow "Adding trigger: $triggerName to ADF publish options for stop/start"
-        $opt.Includes.Add("trigger.$triggerName", "")
-    }
-    else {
-        Write-Debug "$($_.Name) DOES NOT contain $targetPipeline"
-    }    
-}
-
-$opt.DoNotStopStartExcludedTriggers = $true
-$opt.DoNotStopStartExcludedTriggers = $true
+Publish-AdfV2FromJson -RootFolder "$rootFolder" `
+   -ResourceGroupName "$adfResourceGroup" `
+   -DataFactoryName "$adfName" `
+   -Location "$location" `
+   -Option $opt
 
 
-# Publish-AdfV2FromJson -RootFolder "$rootFolder" `
-#    -ResourceGroupName "$adfResourceGroup" `
-#    -DataFactoryName "$adfName" `
-#    -Location "$location" `
-#    -Option $opt
 
-
-function install-adf-dependencies {
-    Install-Module Az.DataFactory -MinimumVersion "1.10.0" -Force
-    Install-Module -Name "azure.datafactory.tools" -Force
-    Import-Module -Name "azure.datafactory.tools" -Force
-}
