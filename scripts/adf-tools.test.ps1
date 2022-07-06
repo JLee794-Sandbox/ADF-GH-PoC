d9d124212dbd1cb0d4db942022beda2a9f77ffbc
Jin Lee <jinle@microsoft.com>
2022-07-06 09:54:17 -0500

    wflow and revamping test env

--- scripts/adf-tools.test.ps1
+++ scripts/adf-tools.test.ps1
@@ -1,7 +1,12 @@
+$rg = "mgb-adf-sandbox"
+$adf = "pocsandbox"
+$location = "eastus"
+$rootFolder = "../root"
+
 $opt = New-AdfPublishOption
 $opt.Excludes.Add("*", "testTrigger1")
 $opt.DoNotStopStartExcludedTriggers = $true
 
 
 # return $opt
-Publish-AdfV2FromJson -RootFolder ../ -ResourceGroupName "validation-rg" -DataFactoryName "ghBranchingPoC" -Location "eastus"
+Publish-AdfV2FromJson -RootFolder $rootFolder -ResourceGroupName $rg -DataFactoryName $adf -Location $location
